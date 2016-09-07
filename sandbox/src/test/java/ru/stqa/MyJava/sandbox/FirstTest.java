7783f014dae363b73aaa6c15a370dc40f4cbc3eb
evg08 <miss.eugenya@yandex.ru>
2016-09-07 13:14:26 +0300

    1

--- sandbox/src/test/java/ru/stqa/MyJava/sandbox/FirstTest.java
+++ sandbox/src/test/java/ru/stqa/MyJava/sandbox/FirstTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 public class FirstTest {
   public void Test1(){
     Point obj2= new Point(5.0,9.0);
+    
     Assert.assertEquals(obj2.distance(),14.560219778561036);
   }
 
