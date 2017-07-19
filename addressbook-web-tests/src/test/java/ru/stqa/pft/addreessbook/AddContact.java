32b20103ebfee29df0536b3019ca81209eea82a3
evg08 <miss.eugenya@yandex.ru>
2017-07-19 11:56:02 +0300

    geckodriver and groups
    добавлено  System.setProperty("webdriver.gecko.driver",
    "C:\\geckodriver\\geckodriver.exe");
    
    добавлен новый модуль для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addreessbook/AddContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addreessbook/AddContact.java
@@ -9,16 +9,16 @@ import org.openqa.selenium.*;
 import ru.stqa.pft.addreessbook.ContactData;
 
 public class AddContact {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.firefox.marionette","G:\\Selenium\\geckodriver-v0.10.0-arm7hf\\geckodriver.exe");
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-      wd.get("http://localhost/addressbook/edit.php");
-      loginPage("admin", "secret");
-    }
+  FirefoxDriver wd;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/edit.php");
+    loginPage("admin", "secret");
+  }
 
   private void loginPage(String username, String password) {
     wd.findElement(By.name("pass")).click();
@@ -33,7 +33,7 @@ public class AddContact {
   }
 
   @Test
-    public void testAddContact() {
+  public void testAddContact() {
 
     goToContactPage();
     fillContact(new ContactData("test1", "test2", "test3", "test4", "test5", "test6", "test7", "1234567", "234567", "1234567", "1987", "wery", "123"));
@@ -74,17 +74,17 @@ public class AddContact {
     wd.findElement(By.name("work")).clear();
     wd.findElement(By.name("work")).sendKeys(contactData.getWorkphone());
     if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[17]")).isSelected()) {
-        wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[17]")).click();
+      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[17]")).click();
     }
     if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).isSelected()) {
-        wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).click();
+      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).click();
     }
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-   wd.findElement(By.name("byear")).sendKeys(contactData.getYearBirthday());
+    wd.findElement(By.name("byear")).sendKeys(contactData.getYearBirthday());
     wd.findElement(By.xpath("//div[@id='content']//label[.='GROUP:']")).click();
     if (!wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).isSelected()) {
-        wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).click();
+      wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).click();
     }
     wd.findElement(By.name("theform")).click();
     wd.findElement(By.name("address2")).click();
@@ -92,7 +92,7 @@ public class AddContact {
     wd.findElement(By.name("address2")).sendKeys(contactData.getAddr());
     wd.findElement(By.name("phone2")).click();
     wd.findElement(By.name("phone2")).clear();
-   wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone3());
+    wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone3());
   }
 
   public void returnToContact() {
@@ -108,16 +108,16 @@ public class AddContact {
   }
 
   @AfterMethod
-    public void tearDown() {
-        wd.quit();
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+  }
 }
